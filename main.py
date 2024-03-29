86e28a7aa5aa1e23d4debc29a42bc6c8991c8a35
iNaneek <118861082+iNaneek@users.noreply.github.com>
2024-03-29 09:13:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 pygame.init()
-screen = pygame.display.set_mode((1920, 1000))
+screen = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption('MERGING BIOG MUSCLESSDS GAMES')
 clock = pygame.time.Clock()
 testFont = pygame.font.Font(None, 85)
@@ -12,20 +12,23 @@ running = True #when set as false, the window closes. used in whileloop further 
 
 class Bros:
     def __init__(self, rank):
-        self.pos = [random.randint(0, 1920), random.randint(0, 1000)]
+        self.pos = [random.randint(0, 1000), random.randint(0, 500)]
         #print(self.pos)
         self.rank = rank
+        self.onMouse = False
     def drawSelf(self):
-        pygame.draw.circle(screen, (0, 0, 0), self.pos, 100)
+        pygame.draw.circle(screen, (0, 0, 0), self.pos, 20)
     
     def move(self):
         self.pos[0] += random.randint(-100, 100)
         self.pos[1] += random.randint(-100, 100)
 
 
+brosDict = {}
+
+brosDict[len(brosDict)] = Bros(0)
 
 
-b1 = Bros(0)
 
 while running:
     screen.fill((255, 255, 255))  # fills screen background
@@ -37,11 +40,17 @@ while running:
         if event.type == pygame.KEYDOWN:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE]:
-                b1.move()
+                brosDict[len(brosDict)] = Bros(0)
+        if pygame.mouse.get_pressed(num_buttons=3)[0] == True:
+            mousePos = pygame.mouse.get_pos()
+            for bro in brosDict:
+                if abs(bro.pos[0] - mousePos[0]) < 10 and abs(bro.pos[1] - mousePos[1]) < 10:
+                    print("rahh")
     
-    b1.drawSelf()
+    for i in brosDict:
+        brosDict[i].drawSelf()
     
-    pygame.draw.circle(screen, (255, 0, 0), (960, 500), 3)
+    pygame.draw.circle(screen, (255, 0, 0), (500, 250), 3)
 
     pygame.display.update()
     clock.tick(60)
